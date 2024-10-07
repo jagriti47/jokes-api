df88e6e07979f56826d35da8d7b60a5ec1743923
Jagriti Chourasia <50318934+jagriti47@users.noreply.github.com>
2024-10-07 14:30:13 +0530

    Delete jokes directory

--- jokes/src/main/java/com/example/jokesapi/repository/JokeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jokesapi.repository;
-
-import com.example.jokesapi.model.Joke;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JokeRepository extends ReactiveCrudRepository<Joke, String> {
-}
